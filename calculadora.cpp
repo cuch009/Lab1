4c741a721f79f68a5e5c8bfa127ca09801be8b7f
cuch009 <159401665+cuch009@users.noreply.github.com>
2024-02-07 21:23:44 -0600

    Add files via upload

---
+++ calculadora.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    float num1, num2, resultado;
+    char operacion;
+
+    cout << "Ingrese el primer numero: ";
+    cin >> num1;
+
+    cout << "Ingrese el segundo numero: ";
+    cin >> num2;
+
+    cout << "Seleccione la operacion (+, -, *, /): ";
+    cin >> operacion;
+
+    switch (operacion) {
+        case '+':
+            resultado = num1 + num2;
+            break;
+        case '-':
+            resultado = num1 - num2;
+            break;
+        case '*':
+            resultado = num1 * num2;
+            break;
+        case '/':
+            if (num2 != 0) {
+                resultado = num1 / num2;
+            } else {
+                cout << "Error: No se puede dividir por cero." << endl;
+                return 1; 
+            }
+            break;
+        default:
+            cout << "Operacion no valida." << endl;
+            return 1; 
+    }
+
+    cout << "El resultado es: " << resultado << endl;
+
+    return 0; 
+}
